dc3ade3e567ceccc1e29e0e83a3a49e56e4e389d
Gomuang <97213554+NewfileDOTpy@users.noreply.github.com>
2023-02-14 22:51:50 +0900

    2023/02/14 Controller 부분 작성

--- NBPOrigin/src/main/java/com/toyProject/NBPOrigin/ApiController/PostController.java
+++ NBPOrigin/src/main/java/com/toyProject/NBPOrigin/ApiController/PostController.java
@@ -1,4 +1,38 @@
 package com.toyProject.NBPOrigin.ApiController;
 
+
+import com.toyProject.NBPOrigin.dto.PostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+    @GetMapping
+    public ResponseEntity postsList(){
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{post-id}")
+    public ResponseEntity show(@PathVariable("post-id") int postid){
+
+
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody PostDto postDto){
+
+
+        return new ResponseEntity(postDto, HttpStatus.CREATED);
+    }
+
+
+
 }
