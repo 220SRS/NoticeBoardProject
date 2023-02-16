664c2f8eb27977722ac620eecf6d58691a559937
Gomuang <97213554+NewfileDOTpy@users.noreply.github.com>
2023-02-16 22:11:53 +0900

    2023/02/16 Controller frame

--- NBPOrigin/src/main/java/com/toyProject/NBPOrigin/ApiController/PostController.java
+++ NBPOrigin/src/main/java/com/toyProject/NBPOrigin/ApiController/PostController.java
@@ -1,6 +1,7 @@
 package com.toyProject.NBPOrigin.ApiController;
 
 import com.toyProject.NBPOrigin.dto.PostDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,4 +10,40 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 
 
+    @GetMapping
+    public ResponseEntity postList(){
+
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/{post-id}")
+    public ResponseEntity show(@PathVariable("post-id") int postId){
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody PostDto postDto){
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+
+
+
+    @PatchMapping("/{post-id}")
+    public ResponseEntity update(@PathVariable("post-id") int postId,
+                                 @RequestBody PostDto postDto){
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{post-id}")
+    public ResponseEntity delete(@PathVariable("post-id") int postID){
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
